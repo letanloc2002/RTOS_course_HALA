822d1a22b046bcbd2d1474a226bea1255bc6724a
Loc Le Tan <letanloc.2002.work@gmail.com>
2025-01-16 23:09:43 +0700

    first commit

---
+++ Assignment/1_RTOS_UART/Src/main.h
@@ -0,0 +1,24 @@
+/*
+ * main.h
+ *
+ *  Created on: Jan 16, 2025
+ *      Author: Loc
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define RCC_APB1ENR		(*(volatile uint32_t*)0x40023840)
+#define USART_SR		(*(volatile uint32_t*)0x40004400)//base
+#define USART_DR		(*(volatile uint32_t*)0x40004404)
+#define USART_BRR		(*(volatile uint32_t*)0x40004408)
+#define USART_CR1		(*(volatile uint32_t*)0x4000440C)
+#define USART_CR1		(*(volatile uint32_t*)0x40004410)
+#define USART_CR1		(*(volatile uint32_t*)0x40004414)
+#define USART_CR1		(*(volatile uint32_t*)0x40004418)
+
+
+
+
+
+#endif /* MAIN_H_ */
---
+++ README.txt
@@ -0,0 +1,2 @@
+Link tài liệu:
+https://drive.google.com/drive/u/1/folders/1wRC6kruT6Sssd82MN4SVukSGGdGBVogE
